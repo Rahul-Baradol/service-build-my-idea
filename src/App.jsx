fd1e58d6bd5e507051f196b5e7b6e5d1af30c074
rahul <rahul.baradol.14@gmail.com>
2024-02-17 14:04:07 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <div className="">Build My Idea !</div>
+    <Navbar />
   )
 }
 
-export default App
+export default App;
